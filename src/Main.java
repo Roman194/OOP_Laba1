1abb289bedb5f7260ebb77afc2ea3448d33b9009
Roman194 <RAZ3113@yandex.ru>
2023-10-04 17:40:30 +0600

    Comments to code were added

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        String modifyingString = sc.nextLine();
-        modifyingString = modifyingString.replaceAll("[^A-Za-z ]", "");
-        System.out.printf(modifyingString);
+        Scanner sc = new Scanner(System.in); //Инициализация объекта класса сканнер для ввода информации с консоли
+        String modifyingString = sc.nextLine(); // Ввод строки из консоли
+        modifyingString = modifyingString.replaceAll("[^A-Za-z ]", ""); // удаление всех небувенных символов, кроме пробелов
+        System.out.printf(modifyingString); // вывод модифицированной строки в консоль
     }
 }
